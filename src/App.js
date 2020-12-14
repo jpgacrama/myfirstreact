7f0080d0339dd71aea6ebaee77c116bc3bee3474
Jonas Gacrama <jonas.gacrama@flexisourceit.com.au>
2020-12-14 13:50:21 +0800

    Fixed spacing

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Car extends React.Component {
-  render() {
-    return <h2>Hi, I am a Car!</h2>;
-   
-}
+    render() {
+        return <h2>Hi, I am a Car!</h2>;
+    }
 }
 
 export default Car;
