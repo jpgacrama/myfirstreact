ffb81caf19239363915bb5aa7a7e7b07a5abf950
Jonas Gacrama <jonas.gacrama@flexisourceit.com.au>
2020-12-14 13:48:55 +0800

    Importing files example

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Car from './App.js';
 
-const myfirstelement = <h1>Hello React!</h1>
-
-ReactDOM.render(myfirstelement, document.getElementById('root'));
+ReactDOM.render(<Car />, document.getElementById('root'));
